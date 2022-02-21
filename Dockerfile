e79918fdef1b8dff9e2b13f7acf3ee44fc794160
yy240 <yijia.yuan@dukekunshan.edu.cn>
2022-02-21 06:39:55 -0500

    update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM python:3.9-slim-bullseye
 RUN mkdir my-model
-ENV MODEL_DIR=/home/jovyan/my-model
+# ENV MODEL_DIR=/home/jovyan/my-model
 ENV MODEL_FILE=model.pkl
-
-
-
 COPY requirements.txt ./requirements.txt
 RUN pip install -r requirements.txt 
 
 COPY app.py ./app.py
+COPY model.pkl ./model.pkl
+
+
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-# Set environnment variables
-MODEL_DIR = os.environ["MODEL_DIR"]
-MODEL_FILE= os.environ["MODEL_FILE"]
-MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
-
 import os
 from flask import Flask
 from flask import request
 from flask import jsonify
 import pickle
 import numpy as np
+from joblib import load
+
+# Set environnment variables
+# MODEL_DIR = os.environ["MODEL_DIR"]
+MODEL_FILE=  os.environ["MODEL_FILE"]
+MODEL_PATH =  MODEL_FILE
 
 app = Flask(__name__)
 model = load(MODEL_PATH)
