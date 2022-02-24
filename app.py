e71812a4fdf290291858be3407677949256f356c
yy240 <yijia.yuan@dukekunshan.edu.cn>
2022-02-24 04:55:51 -0500

    0.93

--- app.py
+++ app.py
@@ -26,6 +26,6 @@ def hello():
     if request.method == 'POST':
         data = request.get_json(force=True)
         country=model.predict(np.array([data['text']]))
-        return jsonify({'is_american':str(country[0]),'version':'0.92','model_date':'2/23'})
+        return jsonify({'is_american':str(country[0]),'version':'0.93','model_date':'2/23'})
     else:
         return "hello"
